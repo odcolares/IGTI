5a39626a365b62bdc550ee265cafd68733f199f8
odcolares <odaircolares@msn.com>
2021-04-08 00:37:51 -0400

    Numpy&Pandas

---
+++ Exercicios/CriandoArrays.py
@@ -0,0 +1,10 @@
+#Criando Arrays 1D
+import numpy as np
+l1 = [1, 2, 3]
+x = np.array(l1)
+print('x: ', x, 'Seu tamanho e :', x.shape)
+
+#Criando Arrays 2D
+l2 = [[1, 2, 3], [4, 5, 6]]
+z = np.array(l2)
+print('z: ', z, 'Seu tamanho e :', z.shape)
