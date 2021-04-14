62ab2941a2f071b96200a2dcc2a2b088bc8ddf27
odcolares <odaircolares@msn.com>
2021-04-13 22:17:24 -0400

    Projeto_WebFlix

---
+++ M2/Exercicios/AcessandoArrays.py
@@ -0,0 +1,32 @@
+import numpy as np
+'''a = np.random.rand(5, 5)
+print('Sua matrix\n', a)
+print('\n')
+print('Acessando seu elemento: ', a[0, 1]) #acessando elemento especifio
+print('\n')
+
+a[1, 1] = 5
+print('Matrix modificada:\n', a) #Add elemento
+print('\n')
+
+b = np.arange(10, 25)
+print('Sua matrix \n', b)
+print('Slice UniDirecional', b[0:8]) #Slice UniDirecional
+
+c = np.random.rand(4, 4)
+print('Sua Matrix Bidirecional\n', c)
+print('\n')
+print('Slice BiDirecional', c[0:3, 2]) #Slice BiDirecional
+'''
+a = []
+s = 0
+for c in range(0, 6):
+    a.append(int(input('Digite...')))
+    s = (0, 1, 5)
+print(s)
+print(a)
+
+
+
+
+
---
+++ M2/Exercicios/numpy.py
@@ -0,0 +1,3 @@
+import numpy as np
+l = np.array([1, 2, 3])
+print(l)
